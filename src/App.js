061cf5f032191082188270d5cd975a8fd1d33763
akram.kazmi <akram.kazmi@niki.ai>
2021-01-21 18:27:03 +0530

    download

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import './App.css';
 
 function App() {
 
+  function downloadTxtFile() {
+    const element = document.createElement("a");
+    const file = new Blob([document.getElementById('myInput').value], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = "myFile.txt";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  }
+
   window.addEventListener("beforeunload", (ev) => {
     ev.preventDefault();
     return ev.returnValue = 'Are you sure you want to close?';
@@ -9,8 +18,10 @@ function App() {
 
   return (
     <div id="rulesformitem" class="formitem" className="home">
-      <div className="notepad">Textpad</div>
-      <div class="textwrapper"><textarea cols="5" rows="35" /></div>
+      <div className="notepad">Textpad
+      <button onClick={downloadTxtFile} className="download">Download</button>
+      </div>
+      <div class="textwrapper"><textarea cols="5" rows="35" id="myInput" /></div>
     </div>
   );
 }
